54200c51f2cfcc71033c10fdb8e7e02373d332e1
Nee Buntoum <101156624+neebuntoum@users.noreply.github.com>
2022-07-16 12:46:10 -0400

    sending index and show data

--- server.js
+++ server.js
@@ -21,5 +21,9 @@ app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
   
